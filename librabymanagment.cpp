833cba6bdba03aa00f5b2e5080df3befb797705f
ptCSTiwari <pushpt109@gmail.com>
2025-06-11 10:34:50 +0530

    librabymanagment.cpp

--- librabymanagment.cpp
+++ librabymanagment.cpp
@@ -1,5 +1,4 @@
-# librabrymanagment
-Built a Library Management System with full functionality to replace manual paperwork and library  processes. Includes 4-book issue limit, 15-day return policy, and conflict management.
+//Built a Library Management System with full functionality to replace manual paperwork and library  processes. Includes 4-book issue limit, 15-day return policy, and conflict management.
 
 #include #include #include #include <unordered_map> #include
 
